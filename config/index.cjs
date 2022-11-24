4afbd55606bad537b8c7d18c26a1853695ddf7c5
gimyuan <78185091+gimyuan@users.noreply.github.com>
2022-11-24 11:20:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o_ZeO5xzfSWqx0n-MScoQMB0OMp8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qsgrSMx6XcPAxbxkPKnRm0UfcYT2Mu80mEL_Rz31XKo',
+      useTemplateId: 'hruKlXbLhUJg_-hqQFRspxtHPbEo4kN1MzOB3CyTrtw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-07',
       festivals: [
